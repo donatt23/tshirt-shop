89df1f56bc8454a7df94e3ab85f0adc97bd3eeeb
donatt23 <donqee2003@hotmail.com>
2022-12-25 16:04:37 +0100

    add scroll to top functionality

--- redirect-btn.js
+++ redirect-btn.js
@@ -5,7 +5,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
       scrollTopBtn.style.display = "block";
    } else {
       scrollTopBtn.style.display = "none";
